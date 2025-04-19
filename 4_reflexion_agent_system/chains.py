0ead5196e687ec74ab681d6914ae2414f339e1ea
Kaushik-Shahare <kaushikshahare0@gmail.com>
2025-04-20 00:03:23 +0530

    Reflexion Agent (Responder Agent)
    Removed .env

---
+++ 4_reflexion_agent_system/chains.py
@@ -0,0 +1,50 @@
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+import datetime
+from langchain_google_genai import ChatGoogleGenerativeAI
+from schema import AnswerQuestion
+from langchain_core.output_parsers.openai_tools import PydanticToolsParser
+from langchain_core.messages import HumanMessage
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+pydantic_parser = PydanticToolsParser(tools=[AnswerQuestion])
+
+# Actor Agent Prompt 
+actor_prompt_template = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """You re expert AI researcher.
+            Current Time: {time}
+            1. {first_instruction}
+            2. Reflext and critique your answer. Be severe to maximize improvement.
+            3. After the reflection, **list 1-3 search queries seperately** for researching improvements. Do not include them inside the reflection.""",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+        ("system", "Answer the user's question above using the required format."),
+    ]
+
+).partial(
+    # Pre populate the variable
+    time=lambda: datetime.datetime.now().isoformat(),
+)
+
+
+llm = ChatGoogleGenerativeAI(model='gemini-1.5-flash')
+
+first_responder_prompt_templete = actor_prompt_template.partial(
+    first_instruction="Provide a detailed ~250 word answer"
+)
+
+# bind_tools help us provide tools to the llm
+# tools: Array of all the tools provided to LLM
+# tool_choice: force the llm to use a particular tool
+first_responder_chain = first_responder_prompt_templete | llm.bind_tools(tools=[AnswerQuestion], tool_choice="AnswerQuestion") | pydantic_parser
+
+response = first_responder_chain.invoke({
+    "messages": [HumanMessage(content="Write me a blog post on how small business can leverage AI to grow")]
+})
+
+print(response)
